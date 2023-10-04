017220f1b7650721c6dd83e387acd622f950b5db
Your Name <you@example.com>
2023-10-04 15:30:36 +0200

    exo2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
 var ds = document.getElementById("ds")
 ds.addEventListener("click", dsClick, false);
+var img1 = document.getElementById("img1")
+img1.addEventListener("click", img1Click, false);
+var img2 = document.getElementById("img2")
+img2.addEventListener("click", img2Click, false);
+var img3 = document.getElementById("img3")
+img3.addEventListener("click", img3Click, false);
+var img4 = document.getElementById("img4")
+img4.addEventListener("click", img4Click, false);
+
+
 
 function dsClick() {
     console.log("clic")
+};
+
+function img1Click() {
+    console.log(img1)
+};
+
+function img2Click() {
+    console.log(img2)
+};
+
+function img3Click() {
+    console.log(img3)
+};
+
+function img4Click() {
+    console.log(img4)
 };
